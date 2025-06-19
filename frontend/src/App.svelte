d7fbaad5213a50152633f834773016fe47a1c276
stelleron <shreyasdonti223@gmail.com>
2025-06-19 17:43:48 +0530

    Added basic form

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -2,22 +2,22 @@
 </script>
 
 <main>
-  <h1 class="text-3xl font-bold underline">Resume Generator</h1>
-  <form>
-        <label>Name</label><br>
-        <input type="text"><br>
+  <fieldset class="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+    <legend class="fieldset-legend">Resume Generator</legend>
 
-        <label>Phone Number</label><br>
-        <input type="text"><br>
-        
-        <label>E-Mail</label><br>
-        <input type="text"><br>
+    <label class="label">Name</label>
+    <input type="text" class="input" placeholder="John Doe" />
 
-        <label>LinkedIn</label><br>
-        <input type="text"><br>
+    <label class="label">Phone Number</label>
+    <input type="text" class="input" placeholder="111-111-1111" />
 
-        <label>GitHub</label><br>
-        <input type="text"><br>
-        
-  </form>
+    <label class="label">Email</label>
+    <input type="text" class="input" placeholder="johndoe@gmail.com" />
+
+    <label class="label">LinkedIn</label>
+    <input type="text" class="input" placeholder="linkedin.com/johndoe" />
+
+    <label class="label">GitHub</label>
+    <input type="text" class="input" placeholder="github.com/johndoe" />
+  </fieldset>
 </main>
