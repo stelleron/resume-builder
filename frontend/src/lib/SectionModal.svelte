06aa810bb7ecc2249f406c3c32def00658b88696
stelleron <shreyasdonti223@gmail.com>
2025-06-27 16:26:12 +0530

    Updated section modal

--- frontend/src/lib/SectionModal.svelte
+++ frontend/src/lib/SectionModal.svelte
@@ -1,13 +1,22 @@
 <main>
     <dialog id="section_modal" class="modal">
-        <div class="modal-box">
-            <h3 class="text-lg font-bold">Hello!</h3>
-            <p class="py-4">Press ESC key or click the button below to close</p>
-            <div class="modal-action">
+        <div class="modal-box relative">
+            <!-- Close icon in top right -->
             <form method="dialog">
-                <!-- if there is a button in form, it will close the modal -->
-                <button class="btn">Close</button>
+                <button class="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
             </form>
+            
+            <h3 class="text-lg font-bold">Add New Section</h3>
+            <fieldset class="fieldset">
+                <legend class="fieldset-legend">Section Name</legend>
+                <input type="text" class="input" placeholder="Section"/>
+            </fieldset>
+                        
+            <div class="modal-action">
+                <form method="dialog">
+                    <!-- if there is a button in form, it will close the modal -->
+                    <button class="btn">Submit</button>
+                </form>
             </div>
         </div>
     </dialog>
