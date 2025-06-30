27815e927b67cf8632d4140a016c55e32e8b0115
stelleron <shreyasdonti223@gmail.com>
2025-06-30 22:08:23 +0530

    Increased width of resume form

--- frontend/src/lib/HeaderForm.svelte
+++ frontend/src/lib/HeaderForm.svelte
@@ -13,23 +13,23 @@
 
 <main>
   <div class="section-box">
-    <fieldset class="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+    <fieldset class="fieldset bg-base-200 border-base-300 rounded-box w-md border p-4">
       <legend class="fieldset-legend">Resume Details</legend>
 
       <label class="label">Name</label>
-      <input type="text" class="input" placeholder="John Doe" bind:value={$data.name}/>
+      <input type="text" class="input w-md" placeholder="John Doe" bind:value={$data.name}/>
 
       <label class="label">Phone Number</label>
-      <input type="text" class="input" placeholder="111-111-1111" bind:value={$data.phone} />
+      <input type="text" class="input w-md" placeholder="111-111-1111" bind:value={$data.phone} />
 
       <label class="label">Email</label>
-      <input type="text" class="input" placeholder="johndoe@gmail.com" bind:value={$data.email}/>
+      <input type="text" class="input w-md" placeholder="johndoe@gmail.com" bind:value={$data.email}/>
 
       <label class="label">LinkedIn</label>
-      <input type="text" class="input" placeholder="linkedin.com/johndoe" bind:value={$data.linkedin}/>
+      <input type="text" class="input w-md" placeholder="linkedin.com/johndoe" bind:value={$data.linkedin}/>
 
       <label class="label">GitHub</label>
-      <input type="text" class="input" placeholder="github.com/johndoe" bind:value={$data.github}/>
+      <input type="text" class="input w-md" placeholder="github.com/johndoe" bind:value={$data.github}/>
 
       <ResumeDetails data={data}></ResumeDetails>
     </fieldset>
