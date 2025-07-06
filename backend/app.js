ae743f511048e83cf420fe4c74478920500c232c
stelleron <shreyasdonti223@gmail.com>
2025-07-06 14:38:13 +0530

    Migrated database

--- backend/app.js
+++ backend/app.js
@@ -8,15 +8,16 @@ const cors = require('cors');
 app.use(cors());
 
 app.use('/api', express.Router()
-.get('/usernames', async (req, res) => {
-  try {
-    const users = await getAllUsernames();
-    res.json(users);
-  } catch (error) {
-    console.error("Error fetching usernames:", error);
-    res.status(500).send("Internal Server Error");
-  }
-}));
+   .get('/usernames', async (req, res) => {
+      try {
+        const users = await getAllUsernames();
+        res.json(users);
+      } catch (error) {
+        console.error("Error fetching usernames:", error);
+        res.status(500).send("Internal Server Error");
+      }
+    })
+);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
