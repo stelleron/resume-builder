f00e57d1d1368cde93d2aded787f433def92dec1
stelleron <shreyasdonti223@gmail.com>
2025-07-07 21:15:29 +0530

    Added testdata request

--- backend/app.js
+++ backend/app.js
@@ -1,24 +1,41 @@
+// Imports
 const express = require("express");
-const { getAllUsernames } = require("./db/queries");
+const { PrismaClient } = require('./generated/prisma'); 
 
+// Basic init
 const app = express();
+const prisma = new PrismaClient();
 const PORT = 3000;
 
+// CORS for using from Svelte
 const cors = require('cors');
 app.use(cors());
 
+// Router
 app.use('/api', express.Router()
-   .get('/usernames', async (req, res) => {
-      try {
-        const users = await getAllUsernames();
-        res.json(users);
-      } catch (error) {
-        console.error("Error fetching usernames:", error);
-        res.status(500).send("Internal Server Error");
-      }
+    // Userdata
+   .get('/userdata', async (req, res) => {
+      const users = await prisma.userData.findMany();
+      res.json(users);
+    })
+   .post('/userdata', async (req, res) => {
+      const { username, password } = req.body;
+      const newUser = await prisma.userData.create({
+        data: { username, password },
+      });
+      res.status(201).json(newUser);
+    })
+
+    // For testing - before adding accounts
+    .get('/testdata', async (req, res) => {
+      const testuser = await prisma.userData.findUnique({
+        where: {id : 1,},
+      })
+      res.json(testuser);
     })
 );
 
+// Listen to indicate it's working
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
