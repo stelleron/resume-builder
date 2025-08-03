33ba1b1715fbe48b907579f6e43ae869f47876ea
stelleron <shreyasdonti223@gmail.com>
2025-08-03 15:51:05 +0530

    Changed log in page

--- frontend/src/routes/login/+page.svelte
+++ frontend/src/routes/login/+page.svelte
@@ -2,8 +2,12 @@
   @import '../../app.css';
 </style>
 
+<script lang="ts">
+  import { goto } from "$app/navigation";
+</script>
+
 <fieldset class="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
-  <legend class="fieldset-legend">Login</legend>
+  <legend class="fieldset-legend">Log In to ResumeGen</legend>
 
   <label class="label">Email</label>
   <input type="email" class="input" placeholder="Email" />
@@ -11,5 +15,5 @@
   <label class="label">Password</label>
   <input type="password" class="input" placeholder="Password" />
 
-  <button class="btn btn-neutral mt-4">Login</button>
+  <button class="btn btn-neutral mt-4" on:click={() => goto('/')}>Login</button>
 </fieldset>
--- frontend/src/routes/signup/+page.svelte
+++ frontend/src/routes/signup/+page.svelte
@@ -3,7 +3,7 @@
 </style>
 
 <fieldset class="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
-  <legend class="fieldset-legend">Login</legend>
+  <legend class="fieldset-legend">Sign Up to ResumeGen</legend>
 
   <label class="label">Email</label>
   <input type="email" class="input" placeholder="Email" />
@@ -11,5 +11,5 @@
   <label class="label">Password</label>
   <input type="password" class="input" placeholder="Password" />
 
-  <button class="btn btn-neutral mt-4">Login</button>
+  <button class="btn btn-neutral mt-4">Signup</button>
 </fieldset>
